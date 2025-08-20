1716585607507fad982127d850a3088a9589ff3b
Melinda Chang <melindachang.hy@gmail.com>
2025-08-20 12:34:09 -0400

    feat: changed foot config for new spec

--- nvim/init.lua
+++ nvim/init.lua
@@ -1031,7 +1031,7 @@ require('lazy').setup({
   },
 })
 
-vim.cmd.colorscheme 'kanagawa-paper'
+vim.cmd.colorscheme 'catppuccin-mocha'
 
 -- The line beneath this is called `modeline`. See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et
