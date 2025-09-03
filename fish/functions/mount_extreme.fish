e98ea3c57132c5d54623fd05bfc4485736e3fde0
Melinda Chang <melindachang.hy@gmail.com>
2025-09-03 16:42:29 -0400

    feat: script to open image directories in yazi

--- fish/functions/mount_extreme.fish
+++ fish/functions/mount_extreme.fish
@@ -1,3 +1,6 @@
 function mount_extreme
-    sudo mount -t exfat -o uid=1000,gid=1000,umask=0022 UUID=E88E-EF74 /mnt/EXTREME_SSD
+    # sudo mount -t exfat -o uid=1000,gid=1000,umask=0022 UUID=E88E-EF74 /mnt/EXTREME_SSD
+    sudo mount -t exfat \
+        -o uid=1000,gid=$(getent group media | cut -d: -f3),umask=0002 \
+        UUID=E88E-EF74 /mnt/EXTREME_SSD
 end
--- fish/functions/usync.fish
+++ fish/functions/usync.fish
@@ -1,3 +1,4 @@
 function usync -a src dest
-    command rsync -aHAXxv --numeric-ids --info=progress2 --bwlimit=20000 hychang@lycus.usbx.me:/home/hychang/downloads/qbittorrent/$src $dest
+    # command rsync -aHAXxv --numeric-ids --info=progress2 --bwlimit=20000 hychang@lycus.usbx.me:/home/hychang/downloads/qbittorrent/$src $dest
+    command rsync -aHAXxv --numeric-ids --info=progress2 hychang@lycus.usbx.me:/home/hychang/downloads/qbittorrent/$src $dest
 end
