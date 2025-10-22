aae4d3821bfbd3964a2bc87d9ead7b10bd72e509
Melinda Chang <melindachang.hy@gmail.com>
2025-10-22 11:54:31 -0500

    feat(hypr): fish function to resize floatingwindows

--- fish/functions/low_battery_alert.fish
+++ fish/functions/low_battery_alert.fish
@@ -1,6 +1,6 @@
 function low_battery_alert
     while true
-        set bat_level (pwr)
+        set bat_level (cat /sys/class/power_supply/BAT0/capacity)
         if test $bat_level -le 20
             notify-send "Battery Low" "Level: $bat_level%"
             sleep 1200
