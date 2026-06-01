cb6785754e16d316c43b7850c1d593315ba11a60
Melinda Chang <melindachang.hy@gmail.com>
2026-06-01 13:03:51 -0500

    chore(helix): switch to helix w/ steel integration

--- fish/config.fish
+++ fish/config.fish
@@ -5,7 +5,6 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
     zoxide init fish | source
     starship init fish | source
-    mise activate fish | source
 end
 
 alias wget="wget --hsts-file=$XDG_DATA_HOME/wget-hsts"
